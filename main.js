b5e2fa012972ba0edde01d2398d7d72f6613cd1d
Sive <siveloveslean@gmail.com>
2025-01-29 11:37:01 +0200

    feat: get choice of the computer and human
    Create a function that generates a random choice between rock, paper, and
    scissors for the computer. Return the choice.
    
    Create a function that takes input of the user's choice between rock,
    paper, and scissors. Return the choice

---
+++ main.js
@@ -0,0 +1,24 @@
+function getComputerChoice() {
+
+  let rndNum = Math.floor(Math.random() * 3); 
+
+  if (rndNum === 0) {
+    return "Rock"; 
+  } else if (rndNum === 1) {
+    return "Paper"; 
+  } else if (rndNum === 2) {
+    return "Scissors"; 
+  }
+}
+
+function getHumanChoice() {
+
+  let input = prompt("Choose: Rock, Paper, or Scissors");
+  let choice = input.trim().toLowerCase(); 
+
+  if (choice === "rock" || choice === "paper" || choice === "scissors") {
+    return choice.charAt(0).toUpperCase() + choice.substring(1, choice.length); 
+  } else {
+    return "Invalid input, choose one of the options."; 
+  }
+}
