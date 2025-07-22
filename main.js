ed635f83cda2ed2463c4e5d16d93f49ac94819a3
Sive <siveloveslean@gmail.com>
2025-07-22 07:53:55 +0200

    Create function to return rock paper or scissors

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("hello, world");
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+    return "rock";
+  } else if (randomNumber === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
